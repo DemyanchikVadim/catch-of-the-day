6b0ee65545211e2140d61b0281acd3f8c0bf92ad
DemyanchikVadim <vadim.demyanchik@gmail.com>
2016-12-04 23:29:26 +0300

    Handling React Proptypes with ES6

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -3,24 +3,24 @@
  */
 import React from 'react';
 
-var Header = React.createClass({
-    render : function () {
-        return (
-            <header className="top">
-                <h1>Catch
+class Header extends React.Component{
+    render() {
+    return (
+        <header className="top">
+            <h1>Catch
                     <span className="ofThe">
                         <span className="of">of</span>
                         <span className="the">the</span>
                     </span>
-                    Day</h1>
-                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
-            </header>
-        )
-    },
+                Day</h1>
+            <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+        </header>
+    )
+}
+}
 
-    propTypes : {
-        tagline : React.PropTypes.string
-    }
-});
+Header.propTypes = {
+    tagline : React.PropTypes.string
+}
 
 export default Header;
