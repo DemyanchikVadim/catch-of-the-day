7c38b5a4df490a8c5b02eb39721bbac1b899e77b
DemyanchikVadim <vadim.demyanchik@gmail.com>
2016-12-04 23:20:03 +0300

    Creating React classes with ES6

--- scripts/components/NotFound.js
+++ scripts/components/NotFound.js
@@ -3,10 +3,10 @@
  */
 import React from 'react';
 
-var NotFound = React.createClass({
-    render: function () {
-        return <h1>Page not found</h1>
-    }
-});
+class NotFound extends React.Component {
+    render() {
+    return <h1>Page not found</h1>
+}
+}
 
 export default NotFound;
